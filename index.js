e4e0bea544a66e562d0dad997cb37b19255e75ab
Efrain May <wwifihack@gmail.com>
2020-11-26 17:09:29 -0600

    variables de entorno creadas

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+require('dotenv').config();
 
 //servidor express
 const app = express();
 
+//directorio publico
+app.use(express.static('public'));
+
 //Rutas 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.json({
         ok: true
     })
-})
+}) */
 
 //escucha peticiones
-app.listen(4000, () => { 
-    console.log(`Servidor corriendo en el puerto ${ 4000 }`);
+app.listen(process.env.PORT, () => { 
+    console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
 });
